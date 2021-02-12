7e89740029a7c86a49f5d8101cb33a45fe914cea
azuzu-gonzales <dovhaichuk.kostiantyn@gmail.com>
2021-02-13 00:04:13 +0200

    initial commit

---
+++ class_and_object/AppHello.java
@@ -0,0 +1,17 @@
+public class AppHello {
+    public static void main(String[] args) {
+    //Constructor      
+    Human kos;
+    kos = new Human();
+
+    //variables
+    kos.name = "Kostiantyn";
+    kos.age = 35;
+    kos.sity = "Kharkiv";
+    kos.education = "civil engineer";
+    kos.goal = "to become a Java Developer";
+
+    kos.speak();
+    
+    }
+}
---
+++ class_and_object/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+
+    String name;
+    int age;
+    String sity;
+    String education;
+    String goal;
+
+    public void speak() {
+        System.out.println("Hello, my name is "+ name);
+        System.out.println("I am "+ age + " years old");
+        System.out.println("I live in the city of " + sity);
+        System.out.println("My education is a "+ education);
+        System.out.println("My goal "+ goal);
+    }
+}

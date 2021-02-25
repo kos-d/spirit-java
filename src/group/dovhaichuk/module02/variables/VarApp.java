bb6db4e017b4a8cb842769e8f17502789f646ec6
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-02-25 10:36:46 +0200

    add variables, all type

--- src/group/dovhaichuk/module02/variables/VarApp.java
+++ src/group/dovhaichuk/module02/variables/VarApp.java
@@ -3,5 +3,21 @@ package dovhaichuk.module02.variables;
 public class VarApp {
     public static void main(String[] args) {
         
+        //Numeric
+        byte b = 8;
+        int i = 32;
+        long l = 64;
+        
+        double d = 32;
+        floar f = 64f;
+
+        //Textual
+        char c = 'a';
+        String s = "some text";
+
+        //Logical
+        boolean bool = true;
+
+        //Referenct - ссылочные, null        
     }
 }

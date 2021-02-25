3fe8f6abb52a6fe19ac105131a11d99df978ee95
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-02-25 11:22:43 +0200

    add println methods

--- src/group/dovhaichuk/module02/variables/VarApp.java
+++ src/group/dovhaichuk/module02/variables/VarApp.java
@@ -1,23 +1,39 @@
-package dovhaichuk.module02.variables;
+//package dovhaichuk.module02.variables;
 
 public class VarApp {
     public static void main(String[] args) {
         
         //Numeric
-        byte b = 8;
-        int i = 32;
-        long l = 64;
-        
-        double d = 32;
-        floar f = 64f;
+        byte b = 8;                 // 8 bits
+        short s = 16;               // 16 bits
+        int i = 32;                 // 32 bits
+        long l = 64;                // 64 bits
+      
+        float f = 32f;              // 32 bits
+        double d = 64;              // 64 bits
 
         //Textual
-        char c = 'a';
-        String s = "some text";
+        char c = 'a';               // 16 bits
+        String str = " bits";       // 
 
         //Logical
-        boolean bool = true;
+        boolean bool = true;        // 1 bits
 
-        //Referenct - ссылочные, null        
+        //Reference - ссылочные, null
+        
+        System.out.println("<< Numeric type >>");
+        System.out.println("bite    = " + b + str);       
+        System.out.println("short   = " + s + str);       
+        System.out.println("int     = " + i + str);       
+        System.out.println("long    = " + l + str);       
+        System.out.println("float   = " + f + str);       
+        System.out.println("double  = " + d + str);
+        
+        System.out.println("<< Textual type >>");        
+        System.out.println("char    = " + c + str);       
+        System.out.println("String this reference type");
+
+        System.out.println("<< Logical type >>");
+        System.out.println("boolean: true or false");
     }
 }

3b41c1dd8015dae0b356a5cac9c7bbf4cec4267a
Vadim-Havrylitse <vfgavrilice@gmail.com>
2021-03-01 21:34:58 +0200

    старт модуля 02

--- src/group/VadimHavrylitse/module01/Friends.java
+++ src/group/VadimHavrylitse/module01/Friends.java
@@ -1,3 +1,4 @@
+package VadimHavrylitse.module01;
 import java.util.Scanner;
 
 public class Friends {
@@ -5,23 +6,17 @@ public class Friends {
     private String name;
     private int age;
     private String proff;
-/*
-    public Friends(String name, int age, String proff){
-        this.name = name;
-        this.age = age;
-        this.proff = proff;
-    }
-*/
+
     public void setFriends() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите попорядку имя, возраст, професию друга:");
         this.name = in.nextLine();
         this.age = in.nextInt();
-        this.proff = in.nextLine();
-    }
+        this.proff = in.next();
+        }
 
     public void printFriends(){
-        System.out.printf("Имя: %10s Возраст: %3d Проффесия:%s\n", name, age, proff);
+        System.out.printf("Имя: %-10s Возраст: %-3d Проффесия:%s\n", name, age, proff);
     }
 
 

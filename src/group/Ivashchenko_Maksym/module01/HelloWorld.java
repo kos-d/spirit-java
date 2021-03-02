31b6a8497d18a3db626f0fedfc8978ef7f688fbd
Maksym <mcs.max72@gmail.com>
2021-03-02 20:17:33 +0200

    Задание 9/47

--- src/group/Ivashchenko_Maksym/module01/HelloWorld.java
+++ src/group/Ivashchenko_Maksym/module01/HelloWorld.java
@@ -2,6 +2,7 @@ package main.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hello World! I was born!");
     }
 }

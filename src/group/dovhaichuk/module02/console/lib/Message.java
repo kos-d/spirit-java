ba8cafead1bcb624236c44da831e1d251cb86a32
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-03-10 22:53:10 +0200

    work module2 complite

--- src/group/dovhaichuk/module02/console/lib/Message.java
+++
@@ -1,9 +0,0 @@
-package module02.console.lib;
-
-public class Message {
-    public void message() {
-
-        System.out.println("Hello, i'm method .message()" +
-               " from package java_test.lib; ");
-    }
-}
---
+++ src/group/dovhaichuk/module03/homework/autocheck/HarekCity.java
@@ -0,0 +1,54 @@
+package module03.homework.autocheck;
+import java.util.Arrays;
+public class HarekCity {
+    //create empty array
+    public String[] createEmptyArray() {
+        String[] result = new String[0];
+        return result;
+    }
+    public int[] createAgeArray(int age1, int age2, int age3, int age4) {
+        int[] ageArray = {age1, age2, age3, age4};
+        return ageArray;
+        //or
+        //return new int[] {age1, age2, age3, age4};
+    }
+    public void fixNames(String[] names, String[] toReplace) {
+        names[1] = toReplace[0];
+        names[3] = toReplace[1];
+    }
+    public String firstAndLastTogether(String[] names1) {
+        String[] words = {" ", "AND", "TOGETHER"};
+        String result = names1[0] + words[0] + words[1] + words[0] + names1[9] + words[0] + words[2];
+        return result; 
+    }
+    /*
+    public String firstAndLastTogether(String[] names) {
+        return names[0] + " AND " + names[9] + " TOGETHER";
+    }
+     */
+    public void changeElectResult(String[] results) {
+        String tmp = results[0];
+        results[0] = results[results.length -1];
+        results[results.length -1] = tmp;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new HarekCity().createEmptyArray();
+        System.out.println(Arrays.toString(arr));
+
+        int[] ageArray = new HarekCity().createAgeArray(10, 20, 30, 40);
+        System.out.println(Arrays.toString(ageArray));
+
+        String[] names = new String[]{"mixa", "ukio", "barek", "krou", "fiha"};
+        String[] toReplace = new String[]{"lopr", "boki"};
+        new HarekCity().fixNames(names, toReplace);
+        System.out.println(Arrays.toString(names));
+
+        String[] names1 = new String[] {"io", "pu", "re", "max", "kok", "mis", "hit", "kio", "pax", "rew"};
+        System.out.println(new HarekCity().firstAndLastTogether(names1));
+
+        String[] results = new String[]{"miho", "hut"};
+        new HarekCity().changeElectResult(results);
+        System.out.println(Arrays.toString(results));
+    }    
+}

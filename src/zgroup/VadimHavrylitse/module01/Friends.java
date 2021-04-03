7cc442555d06968ba84a0ad01c9dc6d20b51f1d8
Vadim-Havrylitse <vfgavrilice@gmail.com>
2021-04-03 17:29:51 +0300

    экспронт comparator, list, iterator,

--- src/zgroup/VadimHavrylitse/module01/Friends.java
+++ src/zgroup/VadimHavrylitse/module01/Friends.java
@@ -1,11 +1,41 @@
-package VadimHavrylitse.module01;
+package zgroup.VadimHavrylitse.module01;
+
+import java.util.Comparator;
 import java.util.Scanner;
 
-public class Friends {
+public class Friends implements Comparable {
 
     private String name;
     private int age;
     private String proff;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setProff(String proff) {
+        this.proff = proff;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getProff() {
+        return proff;
+    }
+
+    public Friends() {
+        setFriends();
+    }
 
     public void setFriends() {
         Scanner in = new Scanner(System.in);
@@ -13,13 +43,21 @@ public class Friends {
         this.name = in.nextLine();
         this.age = in.nextInt();
         this.proff = in.next();
-        }
+    }
 
-    public void printFriends(){
-        System.out.printf("Имя: %-10s Возраст: %-3d Проффесия:%s\n", name, age, proff);
+    @Override
+    public String toString() {
+        return String.format("Имя: %-10s Возраст: %-3d Проффесия:%s", name, age, proff);
     }
 
 
+    @Override
+    public int compareTo(Object o) {
+        Friends other = (Friends) o;
+        return Integer.compare(this.age, other.age);
+    }
 
 
 }
+
+
--- src/zgroup/VadimHavrylitse/module01/SomeFile.java
+++ src/zgroup/VadimHavrylitse/module01/SomeFile.java
@@ -1,79 +1,52 @@
-package VadimHavrylitse.module01;
-import java.util.Scanner;
+package zgroup.VadimHavrylitse.module01;
+
+import java.util.*;
+
 
 class SomeFile {
 
     public static void main(String[] args) {
 
+        List<Friends> friend = new ArrayList<Friends>();
         Scanner in = new Scanner(System.in);
         int i = 1;
-        int k = 0;
-        Friends f1 = new Friends();
-        Friends f2 = new Friends();
-        Friends f3 = new Friends();
-        Friends f4 = new Friends();
-        Friends f5 = new Friends();
-
-        while (i!=3){
+        while (i != 4) {
             System.out.println("Укажите что нужно делать: 1 - добавить друга, " +
                     "2 - показать всех друзей, " +
-                    "3 - закончить программу");
+                    "31/32 - отсортировать по возрасту/по имени, " +
+                    "4 - закончить программу");
             i = in.nextInt();
-            if (i == 1) {
-                k++;
-                switch (k) {
-                    case (1):
-                        f1.setFriends();
-                        break;
-                    case (2):
-                        f2.setFriends();
-                        break;
-                    case (3):
-                        f3.setFriends();
-                        break;
-                    case (4):
-                        f4.setFriends();
-                        break;
-                    case (5):
-                        f5.setFriends();
-                        break;
-                    default:
-                        i = 3;
-                        break;
-                }
+            switch (i) {
+                case 1:
+                    friend.add(new Friends());
+                    break;
+                case 2:
+                    printCollection(friend);
+                    break;
+                case 31:
+                    Collections.sort(friend, new FriendsCompereAge());
+                    break;
+                case 32:
+                    Collections.sort(friend, new FriendsCompereName());
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Вы не правильно ввели цифру!");
+
+
             }
 
-            if (i == 2)
-                for (int j = 0; j < 6; j++){
-                    switch (j) {
-                        case (1):
-                            f1.printFriends();
-                            break;
-                        case (2):
-                            f2.printFriends();
-                            break;
-                        case (3):
-                            f3.printFriends();
-                            break;
-                        case (4):
-                            f4.printFriends();
-                            break;
-                        case (5):
-                            f5.printFriends();
-                            break;
-                    }
-                }
-
-
-
-
-
-
         }
-
-
-
-
-
     }
+
+
+    private static void printCollection(List<Friends> friend) {
+        Iterator<Friends> iterator = friend.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+
 }

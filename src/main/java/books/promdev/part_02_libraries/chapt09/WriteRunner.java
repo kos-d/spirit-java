dd27f9babf066986855ba74fdaeeda628d979050
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-10-25 12:30:12 +0300

    chapt09 WriteRunner

---
+++ src/main/java/books/promdev/part_02_libraries/chapt09/WriteRunner.java
@@ -0,0 +1,24 @@
+
+
+import java.io.*;
+
+public class WriteRunner {
+	public static void main(String[] args) {
+		String pArray[] = { "2007 ", "Java SE 6" };
+		File fbyte = new File("byte.txt");
+		File fsymb = new File("symbol.txt");
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(fbyte);
+			FileWriter fw = new FileWriter(fsymb);
+			for (String a : pArray) {
+				fos.write(a.getBytes());
+				fw.write(a);
+			}
+			fos.close();
+			fw.close();
+		} catch (IOException e) {
+			System.err.println("error file: " + e);
+		}
+	}
+}

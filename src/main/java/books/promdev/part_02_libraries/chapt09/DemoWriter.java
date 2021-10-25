d8b7c046c6e815f1080f8da4152d8bd748a75757
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-10-25 13:34:37 +0300

    chapt09 DemoWriter

---
+++ src/main/java/books/promdev/part_02_libraries/chapt09/DemoWriter.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+
+public class DemoWriter {
+	public static void main(String[] args) {
+		File f = new File("res.txt");
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(f, true);
+		} catch (IOException e) {
+			System.err.println("error open stream " + e);
+			System.exit(1);
+		}
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+		
+		double[] v = { 1.10, 1.2, 1.401, 5.01 };
+		for (double version : v)
+			pw.printf("Java %.2g%n", version);
+		pw.close();
+	}
+}

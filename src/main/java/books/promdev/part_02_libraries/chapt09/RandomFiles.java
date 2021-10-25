7dfe1113235de87d0ba17e872e80b279d69bcbe7
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-10-25 12:41:25 +0300

    chapt09 RandomFile

---
+++ src/main/java/books/promdev/part_02_libraries/chapt09/RandomFiles.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+
+public class RandomFiles {
+	public static void main(String[] args) {
+		double data[] = { 1, 10, 50, 200, 5000 };
+		try {
+			RandomAccessFile rf = new RandomAccessFile("temp.txt", "rw");
+			for (double d : data)
+				rf.writeDouble(d);
+			for (int i = data.length - 1; i >= 0; i--) {
+				rf.seek(i * 8);
+				System.out.println(rf.readDouble());
+			}
+			rf.close();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}

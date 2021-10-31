08fccf0726f46390229724b12f6976e4df9a30e3
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-10-31 18:08:15 +0200

    chapt10 iterator

---
+++ src/main/java/books/promdev/part_02_libraries/chapt10/FindOrder.java
@@ -0,0 +1,19 @@
+package books.promdev.part_02_libraries.chapt10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FindOrder {
+    public List<Order> findBiggerAmountOrder(float bigAmount, List<Order> orders) {
+        ArrayList<Order> bigPrices = new ArrayList(); 
+        Iterator<Order> it = orders.iterator();
+        while (it.hasNext()) {
+            Order current = it.next();
+            if (current.getAmount() >= bigAmount) {
+                bigPrices.add(current);
+            }
+        }
+        return bigPrices;
+    }
+}
---
+++ src/main/java/books/promdev/part_02_libraries/chapt10/RunIterator.java
@@ -0,0 +1,22 @@
+package books.promdev.part_02_libraries.chapt10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunIterator {
+    public static void main(String[] args) {
+        
+        ArrayList<Order> orders = new ArrayList<Order>() {
+            {
+                add(new Order(231, 12.f));
+                add(new Order(389, 2.9f));
+                add(new Order(217, 1.7f));
+                add(new Order(240, 14.f));
+            }
+        };
+        
+        FindOrder fo = new FindOrder();
+        List<Order> res = fo.findBiggerAmountOrder(10f, orders);
+        System.out.println(res);
+    }
+}

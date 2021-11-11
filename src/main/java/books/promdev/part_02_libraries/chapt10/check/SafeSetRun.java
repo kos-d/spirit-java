31d2311c1b78f84078b54436e41566ae787277bd
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-11 23:19:34 +0200

    chapt10 SafeSetRun

---
+++ src/main/java/books/promdev/part_02_libraries/chapt10/check/SafeSetRun.java
@@ -0,0 +1,14 @@
+package books.promdev.part_02_libraries.chapt10.check;
+
+//import books.promdev.part_02_libraries.chapt10.test1.Order;
+//import java.util.Collections;
+//import java.util.HashSet;
+//
+//public class SafeSetRun {
+//    public static void main(String[] args) {
+//        HashSet orders;
+//        orders = new HashSet();
+//        orders = Collections.checkedSet(new HashSet<Order>(), Order.class);
+//        orders.add(new Order(220, 10.1f));
+//    }
+//}

2fd400e5b6b7444b14e0bfde14dd5913f4e29b0e
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-11 18:41:45 +0200

    chapt10 TreeSet

---
+++ src/main/java/books/promdev/part_02_libraries/chapt09/io_test/Delete.java
@@ -0,0 +1,32 @@
+package books.promdev.part_02_libraries.chapt09.io_test;
+
+import java.io.*;
+
+public class Delete {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Format command: java Delete <file or catalog>");
+            System.exit(0);
+        }
+        try {
+            delete(args[0]);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public static void delete(String filename) {
+        File f = new File(filename);
+        
+        if (!f.exists()) fail("Delete: no file or catalog");
+        if (f.isDirectory()) {
+            String[] files = f.list();
+            if (files.length > 0) 
+                fail("Delete: no write: " + filename);
+        }
+        boolean success = f.delete();
+        if (!success) fail("Delete: delete no complete");
+    }
+    protected static void fail(String msg) throws IllegalArgumentException {
+        throw new IllegalArgumentException(msg);
+    }
+}
--- src/main/java/books/promdev/part_02_libraries/chapt10/DemoTreeSet.java
+++ src/main/java/books/promdev/part_02_libraries/chapt10/DemoTreeSet.java
@@ -1,5 +1,28 @@
 package books.promdev.part_02_libraries.chapt10;
 //274
-public class DemoTreeSet {
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+public class DemoTreeSet {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        boolean b;
+        for (int i = 0; i < 6; i++) {
+            list.add((int) (Math.random() * 10) + "Y ");
+        }
+        System.out.println(list + " list");
+        TreeSet<String> set = new TreeSet(list);
+        System.out.println(set + "set");
+        
+        System.out.println(set.comparator());
+        
+        System.out.println(set.last() + " " + set.first());
+        
+        HashSet<String> hSet = new HashSet<>(set);
+        for (String str : hSet) {
+            System.out.println(str + "  " + str.hashCode());
+        }
+    }
 }

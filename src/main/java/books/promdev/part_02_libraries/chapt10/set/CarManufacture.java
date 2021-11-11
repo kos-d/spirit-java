dfc2c24374531381f245c7347b9976fa9faa0a36
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-11 18:46:05 +0200

    chapt10 CarManufacture

---
+++ src/main/java/books/promdev/part_02_libraries/chapt10/set/CarManufacture.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package books.promdev.part_02_libraries.chapt10.set;
+
+/**
+ *
+ * @author t-800
+ */
+public enum CarManufacture {
+    
+}

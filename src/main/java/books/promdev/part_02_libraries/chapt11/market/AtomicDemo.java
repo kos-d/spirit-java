f4c59c6a6400e49f13160eee2c9a40dec9698553
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-12 22:59:50 +0200

    chapt11 AtomicDemo

---
+++ src/main/java/books/promdev/part_02_libraries/chapt11/market/AtomicDemo.java
@@ -0,0 +1,14 @@
+package books.promdev.part_02_libraries.chapt11.market;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AtomicDemo {
+    private static final int NUMBER_BROKERS = 30;
+    public static void main(String[] args) {
+        Market market = new Market(new AtomicLong(100));
+        market.start();
+        for (int i = 0; i < NUMBER_BROKERS; i++) {
+            new Broker(market).start();
+        }
+    }
+}

03c540b00f55879a23fa4f3b8857450da5bdbc4b
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-12 21:59:49 +0200

    chapt11 SimpleThread

---
+++ src/main/java/books/promdev/part_02_libraries/chapt11/daemons/SimpleThread.java
@@ -0,0 +1,23 @@
+package books.promdev.part_02_libraries.chapt11.daemons;
+
+public class SimpleThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            if (isDaemon()) {
+                System.out.println("start daemon-thread");
+                Thread.sleep(10_000);
+            } else {
+                System.out.println("start normal thread");
+            }
+        } catch (InterruptedException e) {
+            System.err.print(e);
+        } finally {
+            if (!isDaemon()) {
+                System.out.println("end normal thread");
+            } else {
+                System.out.println("end daemon-thread");
+            }
+        }
+    }
+}

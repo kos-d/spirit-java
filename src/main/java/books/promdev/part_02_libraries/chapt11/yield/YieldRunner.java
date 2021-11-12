a9f8cb32399fc9c9e381647dcc10c8bffe2d0084
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-12 21:55:42 +0200

    chapt11 YieldRunner_

--- src/main/java/books/promdev/part_02_libraries/chapt11/yield/YieldRunner.java
+++ src/main/java/books/promdev/part_02_libraries/chapt11/yield/YieldRunner.java
@@ -3,13 +3,15 @@ package books.promdev.part_02_libraries.chapt11.yield;
 public class YieldRunner {
     public static void main(String[] args) {
         new Thread() {
+            @Override
             public void run() {
                 System.out.println("start thread 1");
                 Thread.yield();
                 System.out.println("end thread 1");
             }
-        }.start();
+        }.start();        
         new Thread() {
+            @Override
             public void run() {
                 System.out.println("start thread 2");
                 System.out.println("end thread 2");

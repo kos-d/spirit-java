8232b73044902f9ff182fefbfd96cd8674a3210b
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-15 20:20:08 +0200

    guide Empty

---
+++ src/main/java/books/guide/Empty.java
@@ -0,0 +1,11 @@
+package books.guide;
+
+public class Empty {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10;) {
+            System.out.println("loop # " + i);
+            i++;
+        }
+    }
+}

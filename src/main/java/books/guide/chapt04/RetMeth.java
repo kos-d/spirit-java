17d834e42f4e5598a860e39ca379a61dba220673
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 22:37:34 +0200

    guide chapt04 RetMeth: use method range()

---
+++ src/main/java/books/guide/chapt04/RetMeth.java
@@ -0,0 +1,24 @@
+package books.guide.chapt04;
+
+public class RetMeth {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+        
+        int range1, range2;
+        
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+        
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+        
+        range1 = minivan.range();
+        range2 = sportscar.range();
+        
+        System.out.println("Minivan can ride " + minivan.passengers + " passenger on length " + range1 + " mile");
+        System.out.println("Minivan can ride " + sportscar.passengers + " passenger on length " + range2 + " mile");
+    }
+}

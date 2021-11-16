7aa1dff268a18313ebb9313157c3c118c07d8227
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 22:55:36 +0200

    guide chapt04 IsFact

---
+++ src/main/java/books/guide/chapt04/IsFact.java
@@ -0,0 +1,11 @@
+package books.guide.chapt04;
+
+public class IsFact {
+    public static void main(String[] args) {
+        Factor x = new Factor();
+        
+        if (x.isFactor(2, 20)) System.out.println("2 - divider");
+        if(x.isFactor(3, 20))
+            System.out.println("this string not out");
+    }
+}

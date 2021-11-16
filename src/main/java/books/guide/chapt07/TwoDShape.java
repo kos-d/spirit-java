b6baed7372eb5575d876b2f45fa9e9f41899a3c2
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 19:42:50 +0200

    guide chapt07 TwoDShape: add parametric constructor

--- src/main/java/books/guide/chapt07/TwoDShape.java
+++ src/main/java/books/guide/chapt07/TwoDShape.java
@@ -4,6 +4,11 @@ public class TwoDShape {
     private double width;
     private double height;
     
+    TwoDShape(double w, double h) {
+        width = w;
+        height = h;
+    }
+    
     double getWidth() { return width; }
     double getHeight() { return height; }
     void setWidth(double w) { width = w; }

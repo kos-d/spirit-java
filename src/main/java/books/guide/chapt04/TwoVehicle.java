555fdb34e7aa28c5c96e34697bfd60ae4b0092cf
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 21:47:00 +0200

    guide chapt04 TwoVehicle: add new Object

--- src/main/java/books/guide/chapt04/TwoVehicle.java
+++ src/main/java/books/guide/chapt04/TwoVehicle.java
@@ -4,6 +4,8 @@ public class TwoVehicle {
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
         Vehicle sportscar = new Vehicle();
+        Vehicle sedan;
+        sedan = new Vehicle();
         
         int range1, range2;
         

b1574092a6e6df365eb7e2eb86cbbe4b8c207c40
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 18:53:42 +0200

    guide chapt07 TwoDShape

---
+++ src/main/java/books/guide/chapt07/TwoDShape.java
@@ -0,0 +1,10 @@
+package books.guide.chapt07;
+
+public class TwoDShape {
+    double width;
+    double height;
+    
+    void showDim() {
+        System.out.println("Width and height " + width + " and " + height);
+    }
+}

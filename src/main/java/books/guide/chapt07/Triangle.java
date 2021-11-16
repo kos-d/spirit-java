a45f0677e7484f2f2956efac04d884e85336b6e8
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-16 19:32:17 +0200

    guide chapt07 Triangle: add constructor

--- src/main/java/books/guide/chapt07/Triangle.java
+++ src/main/java/books/guide/chapt07/Triangle.java
@@ -3,6 +3,13 @@ package books.guide.chapt07;
 public class Triangle extends TwoDShape {
     String style;
     
+    // constructor
+    Triangle(String s, double w, double h) {
+        setWidth(w);
+        setHeight(h);
+        style = s;
+    }
+    
     double area() {
         return getWidth() * getHeight() / 2;
     }

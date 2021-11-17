1a461141ed977f61aa29a1f7a1b5e749c15414e5
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 20:52:59 +0200

    guide chapt04 BoolDemo

---
+++ src/main/java/books/guide/chapt02/BoolDemo.java
@@ -0,0 +1,17 @@
+package books.guide.chapt02;
+
+public class BoolDemo {
+    public static void main(String[] args) {
+        boolean b;
+        
+        b = false;
+        System.out.println("Value b: " + b);
+        b = true;
+        System.out.println("Value b: " + b);
+        if (b) System.out.println("This instruction work");
+        b = false;
+        if (b) System.out.println("Thi instruction not work");
+        
+        System.out.println("Comparison result 10 > 9: " + (10 > 9));
+    }
+}

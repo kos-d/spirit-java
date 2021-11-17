ae41ffd61be84c516eb7b1fa8b0d437a0032047d
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 20:40:21 +0200

    guide chapt02 CharArithDemo

---
+++ src/main/java/books/guide/chapt02/CharArithDemo.java
@@ -0,0 +1,16 @@
+package books.guide.chapt02;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+        
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+        
+        ch++;
+        System.out.println("now ch contains " + ch);
+        
+        ch = 90;
+        System.out.println("now ch contains " + ch);
+    }
+}

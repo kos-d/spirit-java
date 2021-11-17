a388b17eebecce10d670856a960f851ff70aa3da
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 21:24:02 +0200

    guide chapt02 StrDemo

---
+++ src/main/java/books/guide/chapt02/StrDemo.java
@@ -0,0 +1,9 @@
+package books.guide.chapt02;
+
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("First string\nSecond string");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}

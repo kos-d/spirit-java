d76f3ddf18a89b24af0204cb3ecdc45844ddd175
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 19:46:52 +0200

    guide chapt02 PrimitiveTypeDemo

---
+++ src/main/java/books/guide/chapt02/PrimitiveTypeDemo.java
@@ -0,0 +1,20 @@
+package books.guide.chapt02;
+
+public class PrimitiveTypeDemo {
+
+    public static void main(String[] args) {
+        String[] primitiveType
+                = {"boolean",
+                    "byte",
+                    "char",
+                    "double",
+                    "float",
+                    "int",
+                    "long",
+                    "short"};
+
+        for (String s : primitiveType) {
+            System.out.println(s);
+        }
+    }
+}

20ade2440c2d238ca7d94cdd62eb5f6a9eeabd01
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 23:09:19 +0200

    guide chapt02 RelLogOps

---
+++ src/main/java/books/guide/chapt02/RelLogOps.java
@@ -0,0 +1,25 @@
+package books.guide.chapt02;
+
+public class RelLogOps {
+    public static void main(String[] args) {
+        int i, j;
+        boolean b1, b2;
+        
+        i = 10;
+        j =11;
+        if (i < j) System.out.println("i < j");
+        if (i <= j) System.out.println("i <= j");
+        if (i != j) System.out.println("i != j");
+        
+        if (i == j) System.out.println("not work");
+        if (i >= j) System.out.println("not work");
+        if (i > j) System.out.println("not work");
+        
+        b1 = true;
+        b2 = false;
+        if (b1 & b2) System.out.println("not work");
+        if (!(b1 & b2)) System.out.println("!(b1 & b2): true");
+        if (b1 | b2) System.out.println("b1 | b2: true");
+        if (b1 ^ b2) System.out.println("b1 ^ b2: true");
+    }
+}

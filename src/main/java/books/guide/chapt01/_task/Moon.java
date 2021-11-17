9ec2711c28177fd9dc9861a04b1676418e1567e7
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 19:05:04 +0200

    guide chapt01 task Moon * earth;

---
+++ src/main/java/books/guide/chapt01/_task/Moon.java
@@ -0,0 +1,12 @@
+package books.guide.chapt01._task;
+
+public class Moon {
+    public static void main(String[] args) {
+        double earth, moon, human;
+        human = 82;
+        earth = 0.17;
+        moon = human * earth;
+        
+        System.out.println("Human weighs on moon: " + moon);
+    }
+}

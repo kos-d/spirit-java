0ae699eaf594aef8cb3a97be474e57ca65497d61
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 20:59:28 +0200

    guide chapt02 practice2_1 Sound

---
+++ src/main/java/books/guide/chapt02/practice2_1/Sound.java
@@ -0,0 +1,11 @@
+package books.guide.chapt02.practice2_1;
+
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+        
+        dist = 1100 * 7.2;
+        
+        System.out.println("Distance " + dist + " foots");
+    }
+}

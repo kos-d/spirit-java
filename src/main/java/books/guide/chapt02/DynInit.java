8696fc032d5eca2757fbc012e49d2d7fed159d85
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 21:27:01 +0200

    guide chapt02 DynInit

---
+++ src/main/java/books/guide/chapt02/DynInit.java
@@ -0,0 +1,11 @@
+package books.guide.chapt02;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double radius = 4, height = 5;
+        
+        double volume = 3.1416 + radius * radius * height;
+        
+        System.out.println("Volume: " + volume);
+    }
+}

1be5d17939a75259a80b94d57ba40310ebfe835f
kos-d <dovhaichuk.kostiantyn@gmail.com>
2021-11-17 18:28:33 +0200

    guide chapt01 BlockDemo

---
+++ src/main/java/books/guide/chapt01/BlockDemo.java
@@ -0,0 +1,16 @@
+package books.guide.chapt01;
+
+public class BlockDemo {
+    public static void main(String[] args) {
+        double i, j, d;
+        
+        i = 5;
+        j = 10;
+        
+        if (i != 0) {
+            System.out.println("i no equal 0");
+            d = j / i;
+            System.out.println("j / i equal " + d);
+        }
+    }
+}
